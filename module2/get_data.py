083316eed3bda3cbbdb7f92965a8ce9aa21044b1
CarstenScholle <carstenscholle@mail.weber.edu>
2024-09-16 09:17:12 -0600

    how to load json and csv files in pandas

--- module2/get_data.py
+++ module2/get_data.py
@@ -3,6 +3,7 @@
 import urllib.request
 import zipfile
 import os
+import sys
 SERVER_URL = 'http://icarus.cs.weber.edu/~hvalle/cs4580/data/'
 SAVING_DIRECTORY = "./Downloads"
 
@@ -12,6 +13,7 @@ def download_file(url: str, file_name: str):
     if not os.path.exists(SAVING_DIRECTORY):
         os.makedirs(SAVING_DIRECTORY)
     file_path = os.path.join(SAVING_DIRECTORY, file_name)
+    print("Downloading file: ", file_name )
     urllib.request.urlretrieve(url + file_name, file_path)
 
     if file_name.endswith(".zip"):
@@ -25,12 +27,17 @@ def unzip_file(file_path):
         os.makedirs(SAVING_DIRECTORY)
     with zipfile.ZipFile(file_path, 'r') as zip_file:
         zip_file.extractall(SAVING_DIRECTORY)
+        print("Extracted files: ", zip_file.namelist())
 
 
 def main():
     """Driven Function
     """
     data = 'pandas01Data.zip'
+
+    if len(sys.argv) == 2:
+        download_file(SERVER_URL, sys.argv[1])
+        return
     download_file(SERVER_URL, data)
 
 
